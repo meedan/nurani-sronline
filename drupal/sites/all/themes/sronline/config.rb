270ce85f504a4fc5e9eca2180c6cbf9738b90c0a
James Andres <james.andres@gmail.com>
2012-06-20 17:11:45 -0700

    Refactoring the sronline theme.

--- drupal/sites/all/themes/sronline/config.rb
+++ drupal/sites/all/themes/sronline/config.rb
@@ -3,7 +3,7 @@ require 'sassy-buttons'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "styles"
+css_dir = "css"
 sass_dir = "scss"
 images_dir = "images"
 javascripts_dir = "js"
