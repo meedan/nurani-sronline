3ecd40f4f6f9ef5f35b137d67433d7294d0434f0
unknown <I A  BALOGUN@.(none)>
2012-06-22 03:04:28 +0100

    fix on form labels, logo and blocks

--- drupal/sites/all/themes/sronline/templates/page.tpl.php
+++ drupal/sites/all/themes/sronline/templates/page.tpl.php
@@ -29,7 +29,7 @@
     <div id="header">
         <?php if (!empty($logo)): ?>
           <a href="<?php print $front_page; ?>" class="logo" title="<?php print t('Home'); ?>" rel="home" id="logo">
-            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"/>
+            <img src="<?php print $logo; ?>" alt="<?php //print t('Home'); ?>"/>
           </a>
         <?php endif; ?>
         <div id="search_bar">
