74fcd0e2dfb55dc03a9aa5928cc6440b28e6d369
James Andres <james.andres@gmail.com>
2012-06-27 09:51:47 -0700

    Adding jQuery plugin and scripts to equalize column heights.

---
+++ drupal/sites/all/themes/sronline/js/sronline.js
@@ -0,0 +1,7 @@
+$(function () {
+
+  // Equalize height of floated blocks on some pages by using the equalHeights
+  // jQuery plugin.
+  $('.page-home .center-wrapper .panel-panel .block').equalHeightsPerRow();  
+  $('.page-text-bundles .view-text-bundles .view-content > .item-list').equalHeightsPerRow();
+});
