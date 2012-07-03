092208c322f2245e1b793545954fafac6177d497
James Andres <james.andres@gmail.com>
2012-07-02 17:28:42 -0700

    Improving the experience of Contact Us and Newsletter forms.  Theming the Drupal messages area.

--- drupal/sites/all/themes/sronline/js/include_jquery_172.js
+++ drupal/sites/all/themes/sronline/js/include_jquery_172.js
@@ -1,9 +1,12 @@
 $(function () {
   var tp = Drupal.settings.theme_path;
   $('body')
+    // $, jQuery is replaced with the 1.7.2 version. Old is moved to _$, _jQuery
     .append('<script src="' + tp + '/libs/jquery/jquery-1.7.2.min.js"><\/script>')
+    // chosen.jquery.min.js relies on the 'jQuery' variable being > v1.7.2.
     .append('<script src="' + tp + '/libs/chosen/chosen/chosen.jquery.min.js"><\/script>')
-    // Create a jQuery_172 variable which can be used later in the execution stack
+    // Create a jQuery_172 variable which can be used later in the execution
     .append('<script>var jQuery_172 = jQuery.noConflict(true);</script>');
+    // Now $ and jQuery refer to jQuery-1.3.2 again, @see $.noConflict()
 });
 
